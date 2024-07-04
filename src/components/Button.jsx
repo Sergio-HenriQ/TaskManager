cd6079df34c528971933448f55de51f8ab1ad45f
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-07-04 19:32:16 -0300

    feat: add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,25 @@
+const Button = ({ children, variant }) => {
+  const setVariant = () => {
+    if (variant === "ghost") {
+      return "bg-transparent text-[#818181]"
+    }
+
+    if (variant === "primary") {
+      return "bg-[#00ADB5] text-white"
+    }
+
+    if (variant === "danger") {
+      return "bg-[#EF4444] text-white"
+    }
+  }
+
+  return (
+    <button
+      className={`flex h-max items-center gap-1 rounded-md px-3 py-1 text-xs font-semibold hover:opacity-65 ${setVariant()}`}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
