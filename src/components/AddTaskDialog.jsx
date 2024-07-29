3d6bc2536105b34857f75a22c70ec68969ead3a4
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-07-29 19:39:13 -0300

    feat: add transition to AddTaskDialog

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,44 +1,78 @@
+import "./AddTaskDialog.css"
+
+import { useRef } from "react"
 import { createPortal } from "react-dom"
+import { CSSTransition } from "react-transition-group"
 
 import Button from "./Button"
 import Input from "./Input"
 
 const AddTaskDialog = ({ isOpen, handleClose }) => {
-  if (!isOpen) return null
+  const nodeRef = useRef()
 
-  return createPortal(
-    <div className="fixed bottom-0 left-0 top-0 flex h-screen w-screen items-center justify-center backdrop-blur-sm">
-      <div className="gap-4 rounded-xl bg-white p-5 text-center shadow">
-        <h2 className="text-xl font-semibold text-[#35383E]">Nova Tarefa</h2>
-        <p className="mb-4 mt-1 text-sm text-[#9A9C9F]">
-          Insira as informações abaixo
-        </p>
+  return (
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={isOpen}
+      timeout={500}
+      classNames="add-task-dialog"
+      unmountOnExit
+    >
+      <div>
+        {createPortal(
+          <div
+            ref={nodeRef}
+            className="fixed bottom-0 left-0 top-0 flex h-screen w-screen items-center justify-center backdrop-blur"
+          >
+            <div className="gap-4 rounded-xl bg-white p-5 text-center shadow">
+              <h2 className="text-xl font-semibold text-[#35383E]">
+                Nova Tarefa
+              </h2>
+              <p className="mb-4 mt-1 text-sm text-[#9A9C9F]">
+                Insira as informações abaixo
+              </p>
 
-        <div className="flex w-[336px] flex-col space-y-4">
-          <Input placeholder="Título da tarefa" label="Titulo" id="title" />
-          <Input placeholder="Selecione" label="Horário" id="time" />
-          <Input
-            placeholder="Descreva a tarefa"
-            label="Descrição"
-            id="description"
-          />
-          <div className="flex gap-3">
-            <Button
-              size="large"
-              variant="secondary"
-              className="w-full"
-              onClick={handleClose}
-            >
-              Cancelar
-            </Button>
-            <Button size="large" className="w-full">
-              Salvar
-            </Button>
-          </div>
-        </div>
+              <div className="flex w-[336px] flex-col space-y-4">
+                <Input
+                  placeholder="Título da tarefa"
+                  label="Titulo"
+                  id="title"
+                />
+                <div className="flex flex-col gap-1 text-start">
+                  <select
+                    id="time"
+                    className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]"
+                  >
+                    <option value="morning">Manhã</option>
+                    <option value="afternoon">Tarde</option>
+                    <option value="evening">Noite</option>
+                  </select>
+                </div>
+                <Input
+                  placeholder="Descreva a tarefa"
+                  label="Descrição"
+                  id="description"
+                />
+                <div className="flex gap-3">
+                  <Button
+                    size="large"
+                    variant="secondary"
+                    className="w-full"
+                    onClick={handleClose}
+                  >
+                    Cancelar
+                  </Button>
+                  <Button size="large" className="w-full">
+                    Salvar
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>,
+          document.body
+        )}
       </div>
-    </div>,
-    document.body
+    </CSSTransition>
   )
 }
 
