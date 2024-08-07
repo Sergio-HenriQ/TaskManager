8a732dd5854ecf76644623345e9ed09886cb176b
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-08-06 22:46:34 -0300

    chore: add prop-types to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types"
 import { tv } from "tailwind-variants"
 
 const Button = ({
@@ -33,6 +34,13 @@ const Button = ({
       {children}
     </button>
   )
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.oneOf(["primary", "secondary", "ghost", "danger"]),
+  size: PropTypes.oneOf(["small", "large"]),
+  className: PropTypes.string,
 }
 
 export default Button
