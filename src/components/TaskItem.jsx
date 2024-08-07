5540f3eb239ad0a03f20e7e4fccc48d3d68d5f0a
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-08-06 23:28:44 -0300

    chore: add prop-types to TaskItem component

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types"
+
 import { CheckIcon, GroupIcon, LoaderIcon, TrashIcon } from "../assets/icons"
 import Button from "./Button"
 
@@ -48,6 +50,18 @@ const TaskItem = ({ task, handleCheckboxClick, handleDeleteClick }) => {
       </div>
     </div>
   )
+}
+
+TaskItem.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    time: PropTypes.oneOf(["morning", "afternoon", "evening"]).isRequired,
+    status: PropTypes.oneOf(["done", "in_progress", "not_started"]).isRequired,
+  }).isRequired,
+  handleCheckboxClick: PropTypes.func.isRequired,
+  handleDeleteClick: PropTypes.func.isRequired,
 }
 
 export default TaskItem
