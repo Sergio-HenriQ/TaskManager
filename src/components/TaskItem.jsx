709b3af126341edd846efa1df919d867cd1b91ca
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-08-10 14:48:06 -0300

    feat: add react router dom and initial TaskDetailsPage

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 import { toast } from "sonner"
 
 import { CheckIcon, GroupIcon, LoaderIcon, TrashIcon } from "../assets/icons"
@@ -73,9 +74,9 @@ const TaskItem = ({ task, handleCheckboxClick, onDeleteSucess }) => {
           )}
         </Button>
 
-        <a href="#" className="transition hover:opacity-75">
+        <Link to={`/task/${task.id}`} className="transition hover:opacity-75">
           <GroupIcon />
-        </a>
+        </Link>
       </div>
     </div>
   )
