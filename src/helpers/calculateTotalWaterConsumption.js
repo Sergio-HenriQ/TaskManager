71913da24c6934edfaf3438499737585b32fb204
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-09-09 20:38:15 -0300

    feat: adding the function that calculates current water consumption

--- src/helpers/calculateTotalWaterConsumption.js
+++ src/helpers/calculateTotalWaterConsumption.js
@@ -1,6 +1,15 @@
 export const calculateTotalWaterConsumption = (array) => {
   const quantityConsumed = array.filter((item) => item.status === "done")
+
+  if (quantityConsumed.length == 0) {
+    return {
+      percentage: 0,
+      title: "0",
+    }
+  }
+
   let largerQuantity = quantityConsumed[0]
+
   for (let index = 1; index <= quantityConsumed.length; index++) {
     let currentValue = quantityConsumed[index]
 
