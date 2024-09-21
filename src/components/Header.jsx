ce1a727db2b6da9e7f03a9cfcbc22943d7cf6b36
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-09-21 07:15:32 -0300

    feat: add task deletion functionality to my header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,27 @@
+import { useQueryClient } from "@tanstack/react-query"
 import PropTypes from "prop-types"
 import { useState } from "react"
+import { toast } from "sonner"
 
 import { AddIcon, TrashIcon } from "../assets/icons"
+import { deleteAllTasks } from "../hooks/data/use-delete-tasks"
+import { useGetTasks } from "../hooks/data/use-get-tasks"
 import AddTaskDialog from "./AddTaskDialog"
 import Button from "./Button"
 
 const Header = ({ children }) => {
   const [addTaskDialogIsOpen, setAddTaskDialogIsOpen] = useState(false)
+  const queryClient = useQueryClient()
+
+  const { data: tasks } = useGetTasks()
+
+  const tasksIdsArray = tasks?.map((task) => task.id)
+
+  const handleDeleteAllTasksClick = () => {
+    tasksIdsArray.forEach((id) => deleteAllTasks(id))
+    toast.success("Tarefas deletadas com sucesso.")
+    queryClient.setQueryData("tasks", [])
+  }
 
   return (
     <header>
@@ -18,7 +33,7 @@ const Header = ({ children }) => {
           <h2 className="text-xl font-semibold">{children}</h2>
         </div>
         <div className="flex items-end gap-2">
-          <Button color="ghost">
+          <Button color="ghost" onClick={handleDeleteAllTasksClick}>
             Limpar tarefas
             <TrashIcon />
           </Button>
