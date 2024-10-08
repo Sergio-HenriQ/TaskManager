50f28d16b903c7d5805296a26b716a38d2bc118d
Sergio Henrique <serginho.ssilva0@gmail.com>
2024-10-08 16:25:53 -0300

    chore: ensuring that Jest knows how to handle SVG imports with ?react

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,12 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/.jest/setup-tests.js"],
   moduleNameMapper: {
     "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/.jest/mocks/fileMock.js",
+    "\\.svg\\?react$": "<rootDir>/.jest/mocks/fileMock.js",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
   transform: {
     "^.+\\.jsx?$": "babel-jest",
     "^.+\\.svg$": "jest-transform-stub",
+    "^.+\\.svg\\?react$": "jest-transform-stub",
   },
 }
